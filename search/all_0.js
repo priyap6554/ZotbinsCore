ef03aef758a0b60ce091eed1ac93534f2db549c3
priyap6554 <priyap6554@users.noreply.github.com>
2025-02-05 22:31:04 +0000

    deploy: 83488dbffc40e136f533998c0cd36362f678dbbe

---
+++ search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['action_0',['Doxygen Github Action',['../md__2github_2workspace_2docs_2doxygen.html#autotoc_md7',1,'']]],
+  ['architecture_1',['Software Architecture',['../md__2github_2workspace_2docs_2sw__architecture.html',1,'']]],
+  ['architecture_20overview_2',['Architecture Overview',['../md__2github_2workspace_2docs_2sw__architecture.html#autotoc_md12',1,'']]]
+];
---
+++ search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['background_0',['Background',['../md__2github_2workspace_2docs_2sw__architecture.html#autotoc_md10',1,'']]],
+  ['below_1',['Overall System Diagram (more detail on each task is shown down below)',['../md__2github_2workspace_2docs_2sw__architecture.html#autotoc_md13',1,'']]],
+  ['build_20upload_2',['How to Build &amp; Upload',['..//github/workspace/README.md#autotoc_md2',1,'']]]
+];
